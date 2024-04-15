504266b6116abaddab5a5a92dac383eb22d41c37
Xavier V <31315918+xvraiden@users.noreply.github.com>
2024-04-15 11:30:59 -0400

    frf plotting

---
+++ force_transmissibility_plot.m
@@ -0,0 +1,17 @@
+function force_transmissibility_plot(f,TR,name)
+    % Changing Masss
+    figure ('Name', 'Base Reaction Response Transmissibility'); 
+    sgtitle(name,'FontSize', 18);
+
+    subplot(2, 1, 1)
+    plot(f,rad2deg(angle(TR)),'markerSize',10);
+    title('Phase Angle', 'FontSize', 18);
+    xlabel('Frequency (Hertz)', 'FontSize', 16);
+    ylabel('Phase Angle (Degrees)', 'FontSize', 16);
+
+    subplot(2, 1, 2)
+    plot(f,abs(TR),'markerSize',10);
+    title('Magnitude', 'FontSize', 18);
+    xlabel('Frequency (Hertz)', 'FontSize', 16);
+    ylabel('Magnitude (N/V)', 'FontSize', 16);
+end
--- frequency_response_plot.m
+++ frequency_response_plot.m
@@ -1,29 +1,32 @@
 function frequency_response_plot(f,H,name)
-    % Changing Masss
     figure ('Name', 'Freqeuncy Response Functions');
     sgtitle(name,'FontSize', 18);
     
     subplot(2, 2, 1)
-    plot(f,rad2deg(angle(H)),'markerSize',10);
+    plot(f,rad2deg(angle(H(1,1))),f,rad2deg(angle(H(1,2))),f,rad2deg(angle(H(2,1))),f,rad2deg(angle(H(2,2))),'markerSize',10);
     title('Phase Angle', 'FontSize', 18);
     xlabel('Frequency (Hertz)', 'FontSize', 16);
     ylabel('Phase Angle (Degrees)', 'FontSize', 16);
+    legend("x/F", "q/F", "x/V", "q/V");
     
     subplot(2, 2, 3)
-    semilogy(f,abs(H),'markerSize',10);
+    semilogy(f,abs(H(1,1)),f,abs(H(1,2)),f,abs(H(2,1)),f,abs(H(2,2)),'markerSize',10);
     title('Magnitude', 'FontSize', 18);frequency
     xlabel('Frequency (Hertz)', 'FontSize', 16);
     ylabel('Magnitude (Vout/Vin)', 'FontSize', 16);
+    legend("x/F", "q/F", "x/V", "q/V");
     
     subplot(2, 2, 2)
-    plot(f,imag(H),'markerSize',10);
+    plot(f,imag(H(1,1)),f,imag(H(1,2)),f,imag(H(2,1)),f,imag(H(2,2)),'markerSize',10);
     title('Imaginary', 'FontSize', 18);
     xlabel('Frequency (Hertz)', 'FontSize', 16);
     ylabel('Imaginary (Vout / Vin)', 'FontSize', 16);
+    legend("x/F", "q/F", "x/V", "q/V");
     
     subplot(2, 2, 4)
-    plot(f,real(H),'markerSize',10);
+    plot(f,real(H(1,1)),f,real(H(1,2)),f,real(H(2,1)),f,real(H(2,2)),'markerSize',10);
     title('Real', 'FontSize', 18);
     xlabel('Frequency (Hertz)', 'FontSize', 16);
     ylabel('Real (Vout/Vin)', 'FontSize', 16);
+    legend("x/F", "q/F", "x/V", "q/V");
 end
